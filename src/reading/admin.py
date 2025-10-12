2b2433bcdc3215654a74bcb1178cfa6b15eae6fb
Tonys <info@tony-schiller.com>
2025-10-12 08:37:23 +0200

    first app, reading within the first idea of savin books without frontend.

---
+++ src/reading/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import Book
+
+# Register your models here.
+
+@admin.display(description="Status")
+def colored_status(obj):
+    color = {
+        "TO_READ": "gray",
+        "READING": "orange",
+        "FINISHED": "green",
+    }
+    color = color.get(obj.status, "black")
+    return format_html('<b style="color: {}">{}</b>', color, obj.get_status_display())
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        colored_status,
+        "rating",
+    )
+    list_filter = ("status", "author")      # rechte Filterleiste
+    search_fields = ("title", "author")     # Suchfeld oben
+    ordering = ("-added_on",)               # neueste zuerst
